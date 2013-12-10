52af52a8413123cda4465dc6e79feb32d4fd11c4
Marc-Oliver Teschke <marcoliverteschke@mac.com>
2013-12-10 11:39:31 +0100

    Ask for confirmation before deleting
    Signed-off-by: Marc-Oliver Teschke <marcoliverteschke@mac.com>

--- app/javascript/dolores.js
+++ app/javascript/dolores.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
 	$('.contact-action').click(function(){
 		$(this).parents('.contact').toggleClass('front-facing').toggleClass('back-facing');
 	});
+
+	$('.contact-action-delete').click(function(){
+		return window.confirm("Willst du diesen Kontakt wirklich wirklich richtig echt löschen? Dann ist er nämlich weg, für immer!")
+	});
 });
